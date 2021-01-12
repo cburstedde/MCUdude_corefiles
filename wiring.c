93f2ec0f1f1e11e667f05875d477f72ecec3df25
Carsten Burstedde <burstedde@ins.uni-bonn.de>
2021-01-12 21:31:05 +0100

    Work on delayMicroseconds accepting 16384 as input

--- wiring.c
+++ wiring.c
@@ -404,7 +404,12 @@ void delay(unsigned long ms)
  * Assumes a 1, 1.8432, 2, 3.6864, 4, 7.3728, 8, 9.216, 10, 11.0592, 12, 14.7456, 16,
  * 18.432, 20, 24, 25 or 32 MHz clock. */
 
-#define _MORENOP_ ""
+#define _MORENOP_  ""
+#define _ONE_NOP_  " nop \n\t "
+#define _TWO_NOP_   _ONE_NOP_  _ONE_NOP_
+#define _FOUR_NOP_  _TWO_NOP_  _TWO_NOP_
+#define _FIVE_NOP_  _FOUR_NOP_ _ONE_NOP_
+#define _SIX_NOP_   _FOUR_NOP_ _TWO_NOP_
 
 /* Link time optimization (LTO for short) has been supported by the IDE since v1.6.11.
  * In Arduino IDE 1.6.11 and newer LTO is enabled by default.  The LTO optimizes the code
@@ -438,10 +443,10 @@ void delayMicroseconds(unsigned int us)
   // 32 MHz is two times faster than 16 MHz so need more nop in the
   // wait cycle and keep the same delay capability than 16 MHz
 #undef  _MORENOP_
-#define _MORENOP_ " nop \n\t  nop \n\t  nop \n\t  nop \n\t"
+#define _MORENOP_ _FOUR_NOP_
 
   // account for the time taken in the preceeding commands.
-  // we just burned 16 (18) cycles above, remove 3, (2*8=16)
+  // we just burned 16 (18) cycles above, remove 2, (2*8=16)
   // us is at least 4 so we can substract 2
   us -= 2; // = 2 cycles
 
@@ -451,18 +456,19 @@ void delayMicroseconds(unsigned int us)
   // we catch this case so we don't underrun by subtraction
   if (us == 0) return;           // 3 cycles (.1us) on false, which we ignore
 
-  // the following loop takes a 1/5 of a microsecond (5 cycles)
-  // per iteration, so execute it five times for each microsecond of
+  // the following loop takes a 2/5 of a microsecond (10 cycles)
+  // per iteration, so execute it five times for every two microseconds of
   // delay requested.
-  us = (us << 2) + us; // x5 us, = 7 cycles
+  us = (us << 1) + (us >> 1); // x2.5, = 7 cycles
 
+  // make loop below take 10 cycles
 #undef  _MORENOP_
-#define _MORENOP_ " nop \n\t"
+#define _MORENOP_ _SIX_NOP_
 
   // account for the time taken in the preceeding commands.
-  // we just burned 19 (21) cycles above, remove 4, (4*5=20)
-  // us is at least 5 so we can substract 4
-  us -= 4; // = 2 cycles
+  // we just burned 22 (24) cycles above, remove 2 (2*10=20)
+  // us is at least 2 so we can only subtract 1
+  us -= 1; // = 2 cycles
 
 #elif F_CPU >= 24000000L
   // for the 24 MHz external clock if someone is working with USB
@@ -473,12 +479,16 @@ void delayMicroseconds(unsigned int us)
   // the following loop takes a 1/6 of a microsecond (4 cycles)
   // per iteration, so execute it six times for each microsecond of
   // delay requested.
-  us *= 6; // x6 us, = 9 cycles [{ us = (us<<2)+(us<<1); = 9 cycles too }]
+  us <<= 2; // x4 = 4 cycles
+
+  // make loop take six cycles for 1/4th of a microsecond
+#undef  _MORENOP_
+#define _MORENOP_ _TWO_NOP_
 
   // account for the time taken in the preceeding commands.
-  // we just burned 21 (23) cycles above, remove 5, (5*4=20)
-  // us is at least 6 so we can substract 5
-  us -= 5; // = 2 cycles
+  // we burn 19 (21) cycles above plus 2 below, remove 3, (3*6=18)
+  // us is at least 4 so we can subtract 3
+  us -= 3; // = 2 cycles
 
 #elif F_CPU >= 22118400L
   // this is basically the same as for 11.0592, except multiplying by 6, not 3.
@@ -514,25 +524,25 @@ void delayMicroseconds(unsigned int us)
   }
 
 #elif F_CPU >= 20000000L
-  __asm__ __volatile__ (
-    "nop" "\n\t"
-    "nop" "\n\t"
-    "nop" "\n\t"
-    "nop" "\n\t"
-    "nop"); // just waiting 5 cycles
+  __asm__ __volatile__ ( _FIVE_NOP_ ); // just waiting 5 cycles
+
   // for a one-microsecond delay, simply return.  the overhead
   // of the function call takes 19 (21) cycles, which is 1us
   if (us <= 1) return; //  = 3 cycles, (4 when true)
 
-  // the following loop takes a 1/5 of a microsecond (4 cycles)
-  // per iteration, so execute it five times for each microsecond of
-  // delay requested.
-  us = (us << 2) + us; // x5 us, = 7 cycles
+  // make loop slower by 5/4 such that we must only multiply by 4
+#undef  _MORENOP_
+#define _MORENOP_ _ONE_NOP_
 
-  // account for the time taken in the preceeding commands.
-  // we just burned 27 (29) cycles above, remove 7, (7*4=28)
-  // to 2nd us is at least 10 so we can substract 7
-  us -= 7; // = 2 cycles
+  // the following loop takes a 1/4 of a microsecond (5 cycles)
+  // per iteration, so execute it four times for each microsecond of
+  // delay requested.
+  us <<= 2; // x4 us, = 4 cycles
+
+  // account for the time taken in the preceding commands.
+  // we burn 24 (26) cycles above plus 2 below, remove 5 (5*5=25)
+  // us is at least 8 so we can substract 5
+  us -= 5; // = 2 cycles
 
 #elif F_CPU >= 18432000L
   __asm__ __volatile__ (
